05e1be05a5debaa8e9a3d3972639ffe2b2326fd6
utisco <utiscoder@gmail.com>
2022-10-22 08:38:40 +0700

    add file

---
+++ examples/flutter_counter/lib/counter_observer.dart
@@ -0,0 +1,23 @@
+ 
+import 'package:bloc/bloc.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observes all state changes.
+/// {@endtemplate}
+class CounterObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    // ignore: avoid_print
+    print('${bloc.runtimeType} $change');
+  }
+}
+
+// class CounterObserver extends BlocObserver {
+//   @override
+//   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+//     super.onChange(bloc, change);
+//     print('${bloc.runtimeType}$change');
+//   }
+// }
